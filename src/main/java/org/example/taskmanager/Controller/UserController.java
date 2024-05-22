aebdbca8f01c9b62f5129084d08e4efc64983e6c
jiahow <2094282856@qq.com>
2024-05-22 17:26:32 +0800

    feat: User Register

---
+++ src/main/java/org/example/taskmanager/Controller/UserController.java
@@ -0,0 +1,27 @@
+package org.example.taskmanager.Controller;
+
+import org.example.taskmanager.POJO.DTO.UserDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    ArrayList<UserDTO> users = new ArrayList<>();
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserDTO userDTO) {
+        for (UserDTO user : users) {
+            if (user.getName().equals(userDTO.getName()) && user.getPassword().equals(userDTO.getPassword())) {
+                return "该账号已被注册";
+            }
+        }
+        users.add(userDTO);
+        return "注册成功";
+    }
+}
---
+++ src/main/java/org/example/taskmanager/POJO/DTO/UserDTO.java
@@ -0,0 +1,9 @@
+package org.example.taskmanager.POJO.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    String name;
+    String password;
+}
